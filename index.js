9866870353f171d6fd9bc556f1ce33fad510fd37
joaopedrods1 <joaopedrosouzarodriguesj@gmail.com>
2025-12-27 17:19:55 -0300

    feat: add close menu functionality and improve menu toggle behavior

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 const overlay = document.getElementById('overlay');
+const closeMenuBtn = document.getElementById('close-menu');
 const links = document.querySelectorAll('.links a');
+
+function openMenu() {
+    menu.classList.add('active');
+    overlay.classList.add('active');
+    menuToggle.classList.add('hidden');
+}
 
 function closeMenu() {
     menu.classList.remove('active');
-    menuToggle.classList.remove('active');
     overlay.classList.remove('active');
+    menuToggle.classList.remove('hidden');
 }
 
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menuToggle.classList.toggle('active');
-    overlay.classList.toggle('active');
-});
-
+menuToggle.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 
 links.forEach(link => {
     link.addEventListener('click', closeMenu);
 });
+
